e6c6188d08e114186ab1e9b983bcb6bb35106e8c
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2026-02-26 12:04:01 +0100

    Fix/update packages (#15)
    * fix: update packages
    
    * fix: update dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,30 +36,7 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: {
-      lineaSepolia: process.env.LINEASCAN_API_KEY ?? "",
-      lineaMainnet: process.env.LINEASCAN_API_KEY ?? "",
-      sepolia: process.env.ETHERSCAN_API_KEY ?? "",
-      mainnet: process.env.ETHERSCAN_API_KEY ?? "",
-    },
-    customChains: [
-      {
-        network: "lineaSepolia",
-        chainId: 59141,
-        urls: {
-          apiURL: "https://api-sepolia.lineascan.build/api",
-          browserURL: "https://sepolia.lineascan.build/",
-        },
-      },
-      {
-        network: "lineaMainnet",
-        chainId: 59144,
-        urls: {
-          apiURL: "https://api.lineascan.build/api",
-          browserURL: "https://lineascan.build/",
-        },
-      },
-    ],
+    apiKey: process.env.ETHERSCAN_API_KEY ?? "",
   },
 };
 
